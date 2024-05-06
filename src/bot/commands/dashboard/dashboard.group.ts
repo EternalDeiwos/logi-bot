7a1d24ce3e5537085e5b41113ab1b6c25f6862af
Greg Linklater <glinklater@linklater.no>
2024-05-06 09:28:29 +0200

    refactor: remove old logic to prepare for new specs

--- src/bot/commands/dashboard/dashboard.group.ts
+++
@@ -1,6 +0,0 @@
-import { createCommandGroupDecorator } from 'necord';
-
-export const DashboardCommand = createCommandGroupDecorator({
-  name: 'dashboard',
-  description: 'Manage dashboard messages',
-});
--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const QueryRunnerFactoryProvider = 'QUERY_RUNNER_FACTORY';
-
-// Queues
-export const DashboardUpdateQueue = 'dashboard_update';
