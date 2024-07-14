451604433095799e91f515f1ecb6aa16029fe589
P0et <thebeast.479@gmail.com>
2024-07-14 22:53:48 +0200

    Changed from running linter

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.getOrThrow('APP_PORT')
+  const port = configService.getOrThrow('APP_PORT');
   await app.listen(port);
 }
 bootstrap();
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
-export * from './slug'
-export * from './error-base'
+export * from './slug';
+export * from './error-base';
