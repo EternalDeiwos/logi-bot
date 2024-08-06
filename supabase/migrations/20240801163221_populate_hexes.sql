b32390c6bd51370dfc63da678cad9e9504191b62
Greg Linklater <greg@gryphus.io>
2024-08-06 10:31:47 +0200

    Foxhole API scraping and region schema (#2)
    * chore: clear ambiguity for env file
    
    * feat(supabase): populate hexes and regions
    
    * chore: use a different sql linter
    
    * chore: linux-style line-endings
    
    * feat(supabase): populate war status table
    
    * revert: chore: linux-style line-endings
    This was already set properly
    
    * feat(supabase): current war helper
    
    * feat(supabase): cron job to update war
    
    * chore(supabase): resolve timestamp consistently
    
    * chore(supabase): name hex table consistently with other tables
    
    * chore(supabase): name hex table consistently with other tables
    
    * feat(supabase): add region updates
    
    * feat(supabase): add view for non-deleted regions
    
    * chore(supabase): correct names for privilege grants on helper view
    
    * chore(supabase): typo in comment
    
    * feat(supabase): create points of interest from public region markers
    
    * fix(supabase): devman hex naming inconsistent
    Devmanbad...
    
    * fix(supabase): devman hex naming inconsistent
    Devmanbad...

---
+++ supabase/migrations/20240801163221_populate_hexes.sql
@@ -0,0 +1,71 @@
+-- Create hex table
+DROP TABLE IF EXISTS hex;
+
+CREATE TABLE IF NOT EXISTS
+  hex (hex TEXT NOT NULL, name TEXT NOT NULL);
+
+-- 
+-- Access controls
+ALTER TABLE hex
+ADD CONSTRAINT pk_hex PRIMARY KEY (hex);
+
+ALTER TABLE hex ENABLE ROW LEVEL SECURITY;
+
+GRANT DELETE ON TABLE hex TO "service_role";
+
+GRANT INSERT ON TABLE hex TO "service_role";
+
+GRANT REFERENCES ON TABLE hex TO "service_role";
+
+GRANT
+SELECT
+  ON TABLE hex TO "service_role";
+
+GRANT TRIGGER ON TABLE hex TO "service_role";
+
+GRANT
+TRUNCATE ON TABLE hex TO "service_role";
+
+GRANT
+UPDATE ON TABLE hex TO "service_role";
+
+--
+-- Fetch and populate hex names from map list API
+CREATE
+OR REPLACE FUNCTION populate_hexes () RETURNS INTEGER AS $$
+BEGIN
+
+  -- Yeet
+  TRUNCATE TABLE hex CASCADE;
+
+  -- Fetch hex names from the API and populate hex table
+  INSERT INTO hex
+  SELECT
+    split.hex,
+    -- Drop "Hex" from the name of the hex
+    (
+      CASE 
+        WHEN name[array_length(name, 1)] = 'Hex' THEN array_to_string(name[:array_length(name, 1) -1], ' ')
+        ELSE array_to_string(name, ' ')
+      END
+    ) name
+  FROM (
+    SELECT
+      -- Separate words
+      string_to_array(
+        regexp_replace(
+          trim('"' FROM req.content::text), 
+          E'([a-z])([A-Z])', 
+          E'\\1 \\2','g'
+        ), ' '
+      ) name, 
+      trim('"' FROM req.content::text) hex
+    FROM (
+      SELECT jsonb_array_elements(content::jsonb)#>>'{}' content
+      FROM http_get('https://war-service-live.foxholeservices.com/api/worldconquest/maps')
+    ) req
+  ) split;
+
+  RETURN 1;
+END;
+$$ LANGUAGE plpgsql;
---
+++ supabase/migrations/20240805072125_region_updates.sql
@@ -0,0 +1,119 @@
+--
+-- Create table
+CREATE TABLE IF NOT EXISTS
+  region_update (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    hex_id BIGINT NOT NULL,
+    version BIGINT NOT NULL,
+    war_number BIGINT NOT NULL,
+    data JSONB NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    FOREIGN KEY (war_number) REFERENCES war (war_number) ON DELETE RESTRICT
+  );
+
+CREATE INDEX IF NOT EXISTS updated_at_idx_region_update ON region_update USING btree (updated_at);
+
+CREATE INDEX IF NOT EXISTS war_number_idx_region_update ON region_update USING btree (war_number);
+
+--
+-- Access controls
+ALTER TABLE region_update ENABLE ROW LEVEL SECURITY;
+
+GRANT
+SELECT
+  ON TABLE region_update TO "anon";
+
+GRANT
+SELECT
+  ON TABLE region_update TO "authenticated";
+
+GRANT DELETE ON TABLE region_update TO "service_role";
+
+GRANT INSERT ON TABLE region_update TO "service_role";
+
+GRANT REFERENCES ON TABLE region_update TO "service_role";
+
+GRANT
+SELECT
+  ON TABLE region_update TO "service_role";
+
+GRANT TRIGGER ON TABLE region_update TO "service_role";
+
+GRANT
+TRUNCATE ON TABLE region_update TO "service_role";
+
+GRANT
+UPDATE ON TABLE region_update TO "service_role";
+
+--
+-- Update region
+CREATE
+OR REPLACE FUNCTION update_regions () RETURNS INTEGER AS $$
+  DECLARE r RECORD;
+BEGIN
+  
+  FOR r IN 
+    WITH region AS (
+      SELECT DISTINCT ON (rr.hex_id)
+        rr.hex_id,
+        (
+          -- Devman bad
+          CASE 
+            WHEN rr.hex_name = 'Marban Hollow' THEN 'MarbanHollow'
+            ELSE array_to_string(string_to_array(rr.hex_name, ' '), '') || 'Hex'
+          END
+        ) hex_name,
+        u.version,
+        u.updated_at
+      FROM region rr
+      LEFT OUTER JOIN (
+        SELECT DISTINCT ON (hex_id)
+          hex_id,
+          version,
+          updated_at
+        FROM region_update
+        ORDER BY hex_id, updated_at DESC
+      ) u ON rr.hex_id=u.hex_id
+    ), war AS (
+      SELECT * FROM war_current
+    )
+    SELECT
+      status,
+      region.hex_id,
+      (content::jsonb->'version')::bigint version,
+      war.war_number,
+      content::jsonb#>'{mapItems}' data,
+      to_timestamp((content::jsonb->>'lastUpdated')::bigint / 1000) updated_at
+    FROM 
+      region,
+      war,
+      http((
+        'GET',
+        format('https://war-service-live.foxholeservices.com/api/worldconquest/maps/%s/dynamic/public', region.hex_name),
+        ARRAY[http_header('If-None-Match', format('"%s"', coalesce(region.version::text, '')))],
+        NULL,
+        NULL
+      )::http_request) req
+  
+  LOOP
+
+    CONTINUE WHEN r.status != 200;
+
+    INSERT INTO region_update (hex_id, version, war_number, data, updated_at)
+    SELECT 
+      r.hex_id,
+      r.version,
+      r.war_number,
+      r.data,
+      r.updated_at;
+
+  END LOOP;
+
+  RETURN 1;
+END;
+$$ LANGUAGE plpgsql;
+
+--
+-- Schedule region update every 6 minutes (10 times per hour)
+SELECT
+  cron.schedule ('update_regions', '2-59/6 * * * *', 'SELECT update_region()');
