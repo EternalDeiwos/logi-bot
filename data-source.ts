6b84f9bbaed7201123ac615158ad9090b08b46f6
Greg Linklater <glinklater@linklater.no>
2024-09-14 11:58:43 +0200

    feat(game): database migration for game tables

--- data-source.ts
+++ data-source.ts
@@ -8,6 +8,11 @@ import { Ticket } from 'src/core/ticket/ticket.entity';
 import { CrewLog } from 'src/core/crew/log/crew-log.entity';
 import { Guild } from 'src/core/guild/guild.entity';
 import { CrewShare } from 'src/core/crew/share/crew-share.entity';
+import { War } from 'src/game/war/war.entity';
+import { Region } from 'src/game/region/region.entity';
+import { RegionLog } from 'src/game/region/region-log.entity';
+import { Poi } from 'src/game/poi/poi.entity';
+import { Catalog } from 'src/game/catalog/catalog.entity';
 import * as migrations from 'src/database/migrations';
 
 export const AppDataSource = new DataSource({
@@ -19,7 +24,22 @@ export const AppDataSource = new DataSource({
   database: process.env.POSTGRES_DB,
   schema: process.env.POSTGRES_SCHEMA,
   logging: true,
-  entities: [Team, Crew, CrewMember, ForumTagTemplate, ForumTag, Ticket, CrewLog, Guild, CrewShare],
+  entities: [
+    Team,
+    Crew,
+    CrewMember,
+    ForumTagTemplate,
+    ForumTag,
+    Ticket,
+    CrewLog,
+    Guild,
+    CrewShare,
+    War,
+    Region,
+    RegionLog,
+    Poi,
+    Catalog,
+  ],
   subscribers: [],
   migrations,
   migrationsTableName: 'migrations_history',
