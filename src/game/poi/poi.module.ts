0a9fe7b92c5be74825b12146754b15af8dec82fa
Greg Linklater <glinklater@linklater.no>
2024-11-20 14:00:42 +0100

    feat(poi): rpc method to reset locations

--- src/game/poi/poi.module.ts
+++ src/game/poi/poi.module.ts
@@ -5,14 +5,17 @@ import { RegionModule } from 'src/game/region/region.module';
 import { ExpandedPoi, Poi } from './poi.entity';
 import { ExpandedPoiRepository, PoiRepository } from './poi.repository';
 import { PoiService, PoiServiceImpl } from './poi.service';
+import { PoiController } from './poi.controller';
+import { ApiModule } from 'src/core/api/api.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Poi, ExpandedPoi]), WarModule, RegionModule],
+  imports: [TypeOrmModule.forFeature([Poi, ExpandedPoi]), ApiModule, WarModule, RegionModule],
   providers: [
     PoiRepository,
     ExpandedPoiRepository,
     { provide: PoiService, useClass: PoiServiceImpl },
   ],
+  controllers: [PoiController],
   exports: [PoiService],
 })
 export class PoiModule {}
