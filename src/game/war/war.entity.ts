b15e3584bf2f90d8b48367360f8183a74f4315f7
Greg Linklater <glinklater@linklater.no>
2024-11-20 12:21:43 +0100

    chore(game): expose columns to api

--- src/game/war/war.entity.ts
+++ src/game/war/war.entity.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import { Entity, Column, Unique, PrimaryColumn } from 'typeorm';
 
 export enum WarFaction {
@@ -9,18 +10,23 @@ export enum WarFaction {
 @Entity()
 @Unique('uk_clapfoot_id_war', ['id'])
 export class War {
+  @Expose()
   @PrimaryColumn({ name: 'war_number', type: 'int8', primaryKeyConstraintName: 'pk_war_number' })
   warNumber: string;
 
+  @Expose()
   @Column({ type: 'enum', enumName: 'faction', enum: WarFaction, default: WarFaction.None })
   winner: WarFaction;
 
+  @Expose()
   @Column({ name: 'clapfoot_id' })
   id: string;
 
+  @Expose()
   @Column({ name: 'started_at', type: 'timestamptz' })
   startedAt: Date;
 
+  @Expose()
   @Column({ name: 'ended_at', type: 'timestamptz', nullable: true })
   endedAt?: Date;
 }
