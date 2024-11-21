a856617487559713c1b4545886b50299a08dc8b2
Greg Linklater <glinklater@linklater.no>
2024-11-21 09:50:50 +0100

    chore: upgrade catalog version to infantry-59

--- src/app.config.ts
+++ src/app.config.ts
@@ -36,7 +36,7 @@ const schema = {
   APP_PORT: Joi.number().port().default(8080),
   APP_FOXHOLE_VERSION: Joi.string()
     .regex(/\w+-\d+/)
-    .default('naval-57'),
+    .default('infantry-59'),
   APP_CATALOG_VERSION: Joi.string().regex(/v\d+/).default('v1'),
   APP_QUEUE_RETRY_BACKOFF_BASE: Joi.number().default(2),
   APP_QUEUE_RETRY_BACKOFF_MULTIPLIER: Joi.number().default(1000),
